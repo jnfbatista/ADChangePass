9fbc4055c476fc43b46325591f63bcded7a033cd
jnfbatista <jnfbatista@gmail.com>
2016-03-22 15:08:31 +0000

    Version 2.0 - Generalized the usage of the Password Changer

--- NBChangePass/MainWindow.xaml.cs
+++ NBChangePass/MainWindow.xaml.cs
@@ -21,6 +21,10 @@ namespace NBChangePass
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private const string defaultCycleString = "Cycle";
+        private const string defaultChangeString = "Change";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,33 +32,65 @@ namespace NBChangePass
 
         private void CyclePassword(object sender, RoutedEventArgs e)
         {
+            var uname = username.Text;
+            var pwd = password.Password;
+
+
+            if (cycleOption.IsChecked.Value)
+            {
+                var nCycles = int.Parse(nCycle.Text);
+                Task.Run(() => CyclePasswordAsync(uname, pwd, nCycles));
+            }
+            else
+            {
+                var newPass = newPassword.Password;
+                Task.Run(() => CyclePasswordAsync(uname, pwd, newPass));
+            }
+        }
+
+        /// <summary>
+        /// Changes the password in another Thread
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        private void CyclePasswordAsync(string username, string password, int nCycles = 5)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
             //disable button
-            cycleButton.IsEnabled = false;
-            cycleButton.Content = "Cycling";
+            submit.IsEnabled = false;
+                submit.Content = "Cycling";
+            });
 
             try
             {
                 using (var context = new PrincipalContext(ContextType.Domain))
                 {
-                    using (var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username.Text))
+                    using (var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username))
                     {
 
                         if (user == null)
                         {
-                            cycleButton.Content = "User not found. Try Again.";
-                            cycleButton.IsEnabled = true;
-                            return;
+                            this.Dispatcher.Invoke(() =>
+                            {
+                                submit.Content = "User not found. Try Again.";
+                                submit.IsEnabled = true;
+                                return;
+                            });
                         }
 
-                        var currentPwd = password.Password;
+                        var currentPwd = password;
 
-                        var nextPwd = password.Password;
+                        var nextPwd = password;
 
-                        for (int i = 0; i < 5; i++)
+                        for (int i = 0; i < nCycles; i++)
                         {
                             nextPwd = nextPwd + "1";
 
-                            cycleButton.Content += ".";
+                            this.Dispatcher.Invoke(() =>
+                            {
+                                submit.Content += ".";
+                            });
 
                             user.ChangePassword(currentPwd, nextPwd);
 
@@ -65,18 +101,78 @@ namespace NBChangePass
                         }
 
 
-                        user.ChangePassword(currentPwd, password.Password);
+                        user.ChangePassword(currentPwd, password);
                     }
                 }
 
-                cycleButton.Content = "Cycled!";
-
+                this.Dispatcher.Invoke(() =>
+                {
+                    submit.Content = "Cycled!";
+                });
             }
             catch (PrincipalServerDownException)
             {
-
                 MessageBox.Show("Active Directory Server not found.", "Alert");
-                throw;
+            }
+        }
+
+
+        private void ChangePass(string username, string password, string newPassword)
+        {
+            try
+            {
+                using (var context = new PrincipalContext(ContextType.Domain))
+                {
+                    using (var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username))
+                    {
+
+                        if (user == null)
+                        {
+                            this.Dispatcher.Invoke(() =>
+                            {
+                                submit.Content = "User not found. Try Again.";
+                                submit.IsEnabled = true;
+                                return;
+                            });
+                        }
+
+                        user.ChangePassword(password, new);
+                    }
+                }
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    submit.Content = "Changed!";
+                });
+            }
+            catch (PrincipalServerDownException)
+            {
+                MessageBox.Show("Active Directory Server not found.", "Alert");
+            }
+        }
+
+        private void SetMode(object sender, RoutedEventArgs e)
+        {
+            if (!cycleOption.IsChecked.HasValue || newPasswordLabel == null || newPassword == null)
+                return;
+
+            if (cycleOption.IsChecked.Value)
+            {
+                nCycleLabel.Visibility = Visibility.Visible;
+                nCycleContents.Visibility = Visibility.Visible;
+                newPasswordLabel.Visibility = Visibility.Hidden;
+                newPassword.Visibility = Visibility.Hidden;
+
+                submit.Content = defaultCycleString;
+            }
+            else
+            {
+                nCycleLabel.Visibility = Visibility.Hidden;
+                nCycleContents.Visibility = Visibility.Hidden;
+                newPasswordLabel.Visibility = Visibility.Visible;
+                newPassword.Visibility = Visibility.Visible;
+
+                submit.Content = defaultChangeString;
             }
 
         }
